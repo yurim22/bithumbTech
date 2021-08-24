998b170c313d20e900210587001bc11650af8c07
yurim22 <dek1313@naver.com>
2021-08-24 20:20:19 +0900

    fix: change gitignore file

--- week02/day05-firebase/src/Firebase.js
+++ week02/day05-firebase/src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "apikey",
-  authDomain: "authDomain",
-  projectId: "projectId",
-  storageBucket: "storeBucket",
-  messagingSenderId: "massagingSenderId",
-  appId: "addId",
-  measurementId: "measurementId",
+  apiKey: "AIzaSyDk-QkY8qIJI0FZBix4_u7F7Ywy0dWqQH0",
+  authDomain: "bithumb-tech.firebaseapp.com",
+  projectId: "bithumb-tech",
+  storageBucket: "bithumb-tech.appspot.com",
+  messagingSenderId: "951089870526",
+  appId: "1:951089870526:web:e58265772335b1c53990ef",
+  measurementId: "G-M0TXVZPNT9",
 };
 // Initialize Firebase
 
