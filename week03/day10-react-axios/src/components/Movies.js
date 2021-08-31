536fa1ded5d416f8d4262688b1fc203eaf56cd8e
yurim22 <dek1313@naver.com>
2021-08-31 21:49:09 +0900

    propTypes 추가

--- week03/day10-react-axios/src/components/Movies.js
+++ week03/day10-react-axios/src/components/Movies.js
@@ -1,16 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Movies({ id, year, title, summary, poster, genres }) {
   return (
-    <div className='movie'>
-      <img src={poster} alt={title}/>
-      <h3>Title : {title}</h3> <br /> 
-      <h2>{year}년 작품</h2> <br/> 
-      요약 : {summary} <br/>
+    <div className="movie">
+      <img src={poster} alt={title} />
+      <h3>Title : {title}</h3> <br />
+      <h2>{year}년 작품</h2> <br />
+      요약 : {summary} <br />
       장르 : {genres}
       <hr />
     </div>
   );
 }
 
+Movies.propTypes = {
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 export default Movies;
